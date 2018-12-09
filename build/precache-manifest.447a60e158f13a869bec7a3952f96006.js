4bc4554e7ace179add64e86c5841835a5aa53ce9
Panayiotis Paschalides <paschalp@uni.coventry.ac.uk>
2018-12-09 23:37:01 +0000

    he

---
+++ build/precache-manifest.447a60e158f13a869bec7a3952f96006.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e541187da548eb31fd00",
+    "url": "/static/js/main.e541187d.chunk.js"
+  },
+  {
+    "revision": "613942d59740d19f3b3e",
+    "url": "/static/js/1.613942d5.chunk.js"
+  },
+  {
+    "revision": "e541187da548eb31fd00",
+    "url": "/static/css/main.df421bd6.chunk.css"
+  },
+  {
+    "revision": "2772c5402ce3e645a9d3235b734f023c",
+    "url": "/index.html"
+  }
+];
